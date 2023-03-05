393c74b0f7f20aa16c7de6c142033fb3f84ee5ce
Anton Balabanov <asbbnv@gmail.com>
2023-03-05 15:41:55 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='log-analyzer',
+    version='0.0.1',
+    packages=['tests'],
+    url='https://github.com/balabanas/web-server-log-parser',
+    license='MIT License',
+    author='abalabanov',
+    author_email='asbbnv@gmail.com',
+    description='Web Server\'s Log Analyzer'
+)
